c301dc354045064897fc3dcfb94063fef2692ab6
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2026-01-08 16:53:31 +0100

    feat: add auth guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/datasource';
 import { AccommodationsModule } from './accommodations/accommodations.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
+    HealthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
